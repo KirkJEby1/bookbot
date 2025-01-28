3c654c8d5abd7454636a406dedd74b04510738f8
KirkJEby1 <kirkj.eby@gmail.com>
2025-01-28 09:21:21 -0600

    L10 - count. Lowercase function complete.

--- main.py
+++ main.py
@@ -4,15 +4,24 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        # print(file_contents)
         wordcount(file_contents)
+        lowerthecase(file_contents)
+        
     return
 
 def wordcount(file_contents):
     words = file_contents.split()
     count = len(words)
     print(f"This copy of Frankenstein contains {count} words.")
-    
+    return
+
+def lowerthecase(words):
+    lc_string = words.lower()
+    # print(lc_string)
+    return
+
+
 
     
 
